2a8a46b50341d77fa81f42b949e16141c37f3346
Donghee Han <hdh7485@gmail.com>
2021-03-17 00:31:07 +0900

    Added grid publisher node

--- scripts/grid_publisher.py
+++ scripts/grid_publisher.py
@@ -1,1 +1,57 @@
+#!/usr/bin/env python
 import rospy
+from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
+
+def talker():
+    pub = rospy.Publisher('virtual_grid', OccupancyGrid, queue_size=1)
+    rospy.init_node('grid_publisher', anonymous=True)
+    rate = rospy.Rate(1)
+    header = Header()
+    header.frame_id = 'map'
+    seq = 0
+    resolution = 1.0
+    width = 5.0
+    height = 5.0
+    origin_pose = Pose()
+    origin_pose.position.x = -width/2
+    origin_pose.position.y = -height/2
+    origin_pose.position.z = 0.0
+    origin_pose.orientation.x = 0.0
+    origin_pose.orientation.y = 0.0
+    origin_pose.orientation.z = 0.0
+    origin_pose.orientation.w = 1.0
+    while not rospy.is_shutdown():
+        grid = OccupancyGrid()
+        header.seq = seq
+        header.stamp = rospy.Time.now()
+        grid.header = header
+        grid.info.map_load_time = rospy.Time.now()
+        # The map resolution [m/cell]
+        grid.info.resolution = resolution
+        # Map width [cells]
+        grid.info.width = width
+        # Map height [cells]
+        grid.info.height = height
+        # The origin of the map [m, m, rad].  
+        # This is the real-world pose of the cell (0,0) in the map.
+        grid.info.origin = origin_pose
+
+        # The map data, in row-major order, starting with (0,0).  Occupancy
+        # probabilities are in the range [0,100].  Unknown is -1.
+        grid.data = [100, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0]
+
+        pub.publish(grid)
+        rate.sleep()
+        seq += 1
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
